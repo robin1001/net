b2964c6cd907702f766631b0288aadbbeb6fddf8
Binbin Zhang <811364747@qq.com>
2018-04-25 18:27:01 +0800

    refine time

--- test/net-test.cc
+++ test/net-test.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Personal (Binbin Zhang)
+// Copyright (c) 2017 Personal (Binbin Zhang)
 // Created on 2017-06-07
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
--- utils.h
+++ utils.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Personal (Binbin Zhang)
+// Copyright (c) 2016 Personal (Binbin Zhang)
 // Created on 2016-08-08
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
